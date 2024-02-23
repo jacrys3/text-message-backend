12b24dbf3c104dd7595abc0df9c630acb8f6b000
Jacrys3 <ryse0528@gmail.com>
2024-02-23 13:46:40 -0800

    set up connection to frontend

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # text-message-backend
 Backend for a text-message app
+
+run dev app with node app.js
--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 8080
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.get('/api/data', (req, res) => {
+    res.json({message: 'HI'});
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-})
+});
