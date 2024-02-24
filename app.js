8418d9bef9798abfc91ab21a1d23c332fcf63d1c
Jacrys3 <ryse0528@gmail.com>
2024-02-23 16:03:04 -0800

    added .env file and websocket setup. has err_connection_failed currently

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
-const express = require('express')
-const app = express()
-const port = 8080
+require('dotenv').config();
+
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+const port = process.env.PORT;
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('disconnect', () => {
+        console.log('User disconected');
+    });
+
+    socket.on('send_message', (msg) => {
+        io.emit('receive_message', msg);
+    });
+});
 
 app.get('/api/data', (req, res) => {
     res.json({message: 'HI'});
